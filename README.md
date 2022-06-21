7945965761c80d03a4c2fef7d9abdfabb79a0955
Luis Quiroz <luisdev99@gmail.com>
2022-06-20 21:33:36 -0500

    uibuild

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # phonebook-backend
 Backend del directorio telefónico
 https://polar-mountain-19321.herokuapp.com/
+
+### npm install
+
+### npm run dev
+
+# MangoDB
+
+### node mongo.js <password>
+
+## add person from cmd
+
+### node mongo.js <password> <namePerson> <numberPerson>
+
--- index.js
+++ index.js
@@ -1,132 +1,133 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person') // mongoose
+const handleErrors = require('./middleware/handleErrors')
+const unknownEndpoint = require('./middleware/unknownEndpoint')
 
 const app = express()
 
-app.use(express.static('dist')) // lee lo que se encuentra en la carpeta dist(la parte del Frontend)
+// cors
+app.use(cors())
 
-let persons = [
-    {
-        name: "Gabriel Pérez",
-        number: "33-11-55-99-33",
-        id: 1
-    },
-    {
-        name: "Valentina García",
-        number: "33-22-66-00-44",
-        id: 2
-    },
-    {
-        name: "Esmeralda López",
-        number: "33-33-77-11-55",
-        id: 3
-    },
-    {
-        name: "Gaby Gómez",
-        number: "33-33-77-11-55",
-        id: 4
-    },
-    {
-        name: "Carlos Hernández",
-        number: "33-33-77-11-55",
-        id: 5
-    }
-]
-// const requestLogger = (request, response, next) => {
-//     console.log('Method:', request.method)
-//     console.log('Path:  ', request.path)
-//     console.log('Body:  ', request.body)
-//     console.log('---')
-//     next()
-// }
+//express
+app.use(express.static('dist')) // lee lo que se encuentra en la carpeta dist(la parte del Frontend)
 
 app.use(express.json())//json-parser
 // Sin json-parser, la propiedad body no estaría definida. El json-parser funciona para que tome los datos JSON de una solicitud, los transforme en un objeto JavaScript y luego los adjunte a la propiedad body del objeto request antes de llamar al controlador de ruta.
-
-// middleware con información de la request
-// app.use(requestLogger)
-
-// cors
-app.use(cors())
 
 // morgan
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(p => p.id))
-        : 0
-    return maxId + 1
-}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => response.json(persons))
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            }
+            else {
+                response.status(400).send({ error: 'id does not exist' })
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    console.log(body)
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-    if (persons.some(person => person.name === body.name)) {
-        return response.status(400).json({ error: 'name must be unique' })
-    }
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId()
+        date: new Date()
+    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson.toJSON())
+        })
+        .catch(error => next(error))
+    // if (!body.name) {
+    //     return response.status(400).json({ error: 'name missing' })
+    // }
+    // if (!body.number) {
+    //     return response.status(400).json({ error: 'number missing' })
+    // }
+    // Person.find({ name: body.name }).then(result => {
+    //     console.log(result)
+    //     if (result.length > 0) {
+    //         next() // put
+    //     } else {
+    //         console.log('new person')
+    //         const person = new Person({
+    //             name: body.name,
+    //             number: body.number,
+    //             date: new Date()
+    //         })
+    //         person.save()
+    //             .then(savedPerson => {
+    //                 response.json(savedPerson)
+    //             })
+    //             .catch(error => console.log('error', error))
+    //     }
+    // })
+    //     .catch(error => console.log('error', error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { id } = request.params
+    const body = request.body
+    console.log('same name, updating')
+    const newPersonInfo = {
+        number: body.number,
+        date: new Date()
     }
-    persons = persons.concat(person)
-    response.json(person)
+    Person.findByIdAndUpdate(id, newPersonInfo, { new: true, runValidators: true })
+        // new: true    devuelve la información del objeto actualizado, si estuviera en falso, devuelve el objeto encontrado en la base de datos 
+        // runValidators: true  hace que se vuelvan a validar los requisitos del campo
+        .then(personUpdate => {
+            response.json(personUpdate)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    const countPersons = persons.length
-    const info = `
-        Phonebook has info for ${countPersons} people 
-        <br> 
-        ${new Date()}`
-    response.send(info)
+app.get('/info', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            const countPersons = persons.length
+            const info = `
+            Phonebook has info for ${countPersons} people 
+            <br> 
+            ${new Date()}`
+            response.send(info)
+        })
+        .catch(error => next(error))
 })
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
 
 // este middleware se usa para capturar solicitudes realizadas a rutas inexistentes.
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+app.use(handleErrors)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
